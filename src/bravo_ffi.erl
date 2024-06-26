8c3334ec72a12c4a6652007f718613290bb30807
Michael Mark <michael.mark@oit.edu>
2024-06-26 01:40:31 -0700

    Changed get_error_type erlang function

--- src/bravo_ffi.erl
+++ src/bravo_ffi.erl
@@ -11,16 +11,16 @@ inform(Name, Key) ->
   {_, Target} = lists:keyfind(Key, 1, Info),
   Target.
 
-get_error_type(Name, Reason) ->
+get_error_type(Reason) ->
   case Reason of
-      badarg ->
-       case ets:info(Name) == undefined of
-         true -> {error, table_does_not_exist};
-         false -> {error, access_denied}
-       end;
-      _ -> {error, {erlang_error, atom_to_binary(Reason)}}
-    end.
-
+    {badarg, Trace} ->
+      case map_get(cause, element(2, lists:nth(1, element(4, lists:nth(1, Trace))))) of
+        id -> {error, table_does_not_exist};
+        access -> {error, access_denied};
+        _ -> {error, {erlang_error, badarg}}
+      end;
+    _ -> {error, {erlang_error, atom_to_binary(Reason)}}
+  end.
 
 try_new(Name, Options) ->
   case catch ets:new(Name, Options) of
@@ -37,19 +37,19 @@ try_new(Name, Options) ->
 
 try_insert(Name, Key, Value) ->
   case catch ets:insert(Name, {Key, Value}) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     _ -> {ok, nil}
   end.
 
 try_insert_list(Name, Objects) ->
   case catch ets:insert(Name, Objects) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     _ -> {ok, nil}
   end.
 
 try_insert_new(Name, Key, Value) ->
   case catch ets:insert_new(Name, {Key, Value}) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     _ -> {ok, nil}
   end.
 
@@ -58,7 +58,7 @@ try_insert_new_list(Name, Object) ->
 
 try_lookup(Name, Key) ->
   case catch ets:lookup(Name, Key) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     Other ->
       case lists:map(fun(Elem) -> element(2, Elem) end, Other) of
         [] -> {error, empty};
@@ -68,7 +68,7 @@ try_lookup(Name, Key) ->
 
 try_take(Name, Key) ->
   case catch ets:take(Name, Key) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     Other ->
       case lists:map(fun(Elem) -> element(2, Elem) end, Other) of
         [] -> {error, empty};
@@ -78,31 +78,31 @@ try_take(Name, Key) ->
 
 try_member(Name, Key) ->
   case catch ets:member(Name, Key) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     Other -> {ok, Other}
   end.
 
 try_delete(Name) ->
   case catch ets:delete(Name) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     _ -> {ok, nil}
   end.
 
 try_delete_key(Name, Key) ->
   case catch ets:delete(Name, Key) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     _ -> {ok, nil}
   end.
 
 try_delete_object(Name, Object) ->
   case catch ets:delete_object(Name, Object) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     _ -> {ok, nil}
   end.
 
 try_delete_all_objects(Name) ->
   case catch ets:delete_all_objects(Name) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     _ -> {ok, nil}
   end.
 
@@ -136,20 +136,20 @@ try_file2tab(Filename, Verify) ->
 
 try_tab2list(Name) ->
   case catch ets:tab2list(Name) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     Other -> {ok, Other}
   end.
 
 try_first(Name) ->
   case catch(lists:map(fun(Elem) -> element(1, Elem) end, ets:first(Name))) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     '$end_of_table' -> {error, end_of_table};
     Other -> {ok, Other}
   end.
 
 try_last(Name) ->
   case catch(lists:map(fun(Elem) -> element(1, Elem) end, ets:last(Name))) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     '$end_of_table' -> {error, end_of_table};
     Other -> {ok, Other}
   end.
@@ -157,14 +157,14 @@ try_last(Name) ->
 
 try_next(Name, Key) ->
   case catch(lists:map(fun(Elem) -> element(1, Elem) end, ets:next(Name, Key))) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     '$end_of_table' -> {error, end_of_table};
     Other -> {ok, Other}
   end.
 
 try_prev(Name, Key) ->
   case catch(lists:map(fun(Elem) -> element(1, Elem) end, ets:next(Name, Key))) of
-    {'EXIT', {Reason, _}} -> get_error_type(Name, Reason);
+    {'EXIT', Reason} -> get_error_type(Reason);
     '$end_of_table' -> {error, end_of_table};
     Other -> {ok, Other}
   end.
